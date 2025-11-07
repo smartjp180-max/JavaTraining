ba918f33e820e864d5b08325704cbda396ad9dd5
vivek <vikashvivek0391@gmail.com>
2025-11-07 16:31:53 +0530

    first commit

---
+++ Practiceproject/src/package1/Class2.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		int v=89;
+		int k=1;
+		int z =v+k;
+		String s = "mouli4# ";
+		System.out.println("niresh");
+
+	}
+
+}
